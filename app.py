e26d71a91422a2678655f739b0d445052b28828d
James <james@hux.com>
2023-06-03 11:52:35 -0400

    first

---
+++ app.py
@@ -0,0 +1,44 @@
+from io import BytesIO
+from flask import Flask, request
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+import requests
+
+interpreter = tf.lite.Interpreter(model_path='model.tflite')
+interpreter.allocate_tensors()
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+input_shape = input_details[0]['shape']
+app = Flask(__name__)
+
+@app.route("/classify")
+def get_users():
+    url = request.args.get('url')
+    print('Classify URL:', url)
+    image_data = requests.get(url).content
+
+    # Preprocess the image
+    image = Image.open(BytesIO(image_data))
+    resized_image = image.resize((input_shape[1], input_shape[2]))
+    if resized_image.mode != "RGB":
+        resized_image = resized_image.convert("RGB")
+    input_data = np.array(resized_image, dtype=np.float32)
+    input_data = (input_data - 127.5) / 127.5  # Normalize the input image
+    input_data = np.expand_dims(input_data, axis=0)  # Add batch dimension
+
+    # Run inference
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+    interpreter.invoke()
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+
+    print(output_data)
+
+    if output_data[0] > 0.5:
+        return "true"
+    else:
+        return "false"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
